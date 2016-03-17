57b58e6105de26ba4eaaf0b9bac5709f953f28e3
MBorne <mborne@users.noreply.github.com>
2016-03-18 00:29:48 +0100

    ban - nettoyage après import

--- datasets/ban/import-dep.sh
+++ datasets/ban/import-dep.sh
@@ -12,3 +12,8 @@ wget -nc -O $FILENAME $URL
 unzip -d data -o $FILENAME
 shp2pgsql -a -s 4326 -g geometry data/BAN_odbl_$INSEE_DEP.shp ban.adresse > data/adresse-$INSEE_DEP.sql
 psql --quiet -d apicarto -f data/adresse-$INSEE_DEP.sql
+
+rm data/BAN_odbl_*
+rm data/*.sql
+
+
